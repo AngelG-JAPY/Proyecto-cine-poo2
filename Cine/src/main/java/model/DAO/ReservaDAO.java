cfebfbfd9dd95bc2a9be15c9c361a27f01bb8996
JavierLo24 <javierandres.123@hotmail.com>
2021-11-20 16:59:15 -0500

    Nmms se me olvidó guardar lo otro xd

--- Cine/src/main/java/model/DAO/ReservaDAO.java
+++ Cine/src/main/java/model/DAO/ReservaDAO.java
@@ -15,8 +15,8 @@ import model.Red.BaseDeDatos;
 
 
 public class ReservaDAO {
-    public static final String SQL_CONSULTA = "SELECT id_reserva, id_funcion, id_comida, id_usuario, fecha_funcion, fecha_reserva, precio_reserva, cantidad_sillas FROM reserva u\n" +
-                                              "JOIN membresia m ON (u.id_membresia = m.id)";
+    public static final String SQL_CONSULTA = "SELECT r.id_reserva, r.id_funcion, r.id_comida, u.documento, u.nombre, u.genero, u.email, u.contraseña, u.telefono, m.id, m.nombre as nombre_membresia, m.valor, r.fecha_funcion, r.fecha_reserva, r.precio_reserva, r.cantidad_sillas FROM reserva u\n" +
+                                              "JOIN usuario u ON (r.id_usuario = u.documento), JOIN membresia m ON (u.id_membresia = m.id)";
     public static final String SQL_INSERT = "INSERT INTO reserva (id_reserva, id_funcion, id_comida, id_usuario, fecha_funcion, fecha_reserva, precio_reserva, cantidad_sillas) VALUES (?, ?, ?, ?, ?, ?, ?, ?)";
     public static final String SQL_DELETE = "DELETE FROM reserva WHERE id_reserva = ?";
     public static final String SQL_UPDATE = "UPDATE reserva SET id_funcion = ?, id_comida = ?, id_usuario = ?, fecha_funcion = ?, fecha_reserva = ?, precio_reserva = ?, cantidad_sillas = ?, WHERE id_reserva = ?";
@@ -116,17 +116,26 @@ public class ReservaDAO {
            res = ps.executeQuery();
            while (res.next()){
                Reserva r = new Reserva ();
+               Usuario u = new Usuario();
                Membresia m = new Membresia ();
-               s.setId_usuario(res.getInt("documento"));
-               s.setNombre(res.getString("nombre"));
-               s.setGenero(res.getString("genero"));
-               s.setEmail(res.getString("correo"));
-               s.setContraseña(res.getString("contraseña"));
-               s.setTelefono(res.getInt("telefono"));
+               r.setId_reserva(res.getInt("id_reserva"));
+               r.setId_funcion(res.getInt("id_funcion"));
+               r.setId_comida(res.getInt("id_comida"));   
+               u.setId_usuario(res.getInt("documento"));
+               u.setNombre(res.getString("nombre"));
+               u.setGenero(res.getString("genero"));
+               u.setEmail(res.getString("correo"));
+               u.setContraseña(res.getString("contraseña"));
+               u.setTelefono(res.getInt("telefono"));
                m.setId_membresia(res.getInt("id"));
                m.setNombre(res.getString("nombre_membresia"));
                m.setValor(res.getInt("valor"));
-               s.setMembresia(m);
+               u.setMembresia(m);
+               r.setId_usuario(u);
+               r.setFecha_funcion(res.getDate("fecha_funcion"));
+               r.setFecha_reserva(res.getDate("fecha_reserva"));
+               r.setPrecio_reserva(res.getInt("precio_reserva"));
+               r.setCantidad_sillas(res.getInt("cantidad_sillas"));
            }
         } catch (SQLException ex) {
             ex.printStackTrace(System.out);
